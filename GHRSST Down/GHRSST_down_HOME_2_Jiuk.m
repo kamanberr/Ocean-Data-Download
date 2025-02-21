faf35ec48a17f60c346834468ac3c9c562172256
Jiuk Hwang <114406454+kamanberr@users.noreply.github.com>
2025-02-21 10:27:24 +0900

    Add description for download GHRSST

--- GHRSST Down/GHRSST_down_HOME_2_Jiuk.m
+++ GHRSST Down/GHRSST_down_HOME_2_Jiuk.m
@@ -3,6 +3,23 @@
 %   LOG
 %       [2024.10.03] Draft done.
 %       [2024.10.05] Modify url adress.
+%       [2025.02.21] Add description.
+%
+% < How to use this code? >
+%   1. Set the small area and just one day to prepare.
+%   2. After running the code, you see the white internet window with "Login."
+%   3. You should "Log in" for "NASA Earthdata (https://www.earthdata.nasa.gov/)" for the first time. 
+%   4. After "Log in", you set the area and period you want. And just run!
+% 
+% Target data : OSITA v2, OSTIA Reprocessed, MUR 4.1, MUR 4.2 and OISST v2
+% 
+%   - You can set the Area and Period of SST data.
+%   - The sources of OSTIA and MUR are PO.DAAC Homepage.
+%   - The source of OISST is psl.noaa
+%   - OSTIA v2 in PO.DAAC and OSTIA Analysis in CMEMS seem to same dataset.
+%   - The PDF file "Manual for download data from PODAAC using MATLAB (with specific Area and Period)" 
+%       is the way that how I wrote the MATLAB script for downloading data from PODAAC. 
+%       If you want more data from PODAAC, you could write a new MATLAB script using the manual.
 %
 %% OSTIA & MUR 
 %
@@ -16,7 +33,7 @@
 %   Link : https://podaac.jpl.nasa.gov/dataset/OSTIA-UKMO-L4-GLOB-REP-v2.0
 %   Spatial     Resolution  : 0.05° 
 %   Temporal    Resolution  : Daily
-%   Available   Period      : 1982-Jan-01 to 2022-Jun-01
+%   Available   Period      : 1982-Jan-01 to 2024-Jan-01
 %--------------------------------------------------------------------------
 % [3] MUR SST Analysis 4.1                  | MUR-JPL-L4-GLOB-v4.1
 %   Link : https://podaac.jpl.nasa.gov/dataset/MUR-JPL-L4-GLOB-v4.1
@@ -33,10 +50,10 @@
 clc; clear;
 
 % OSITA v2 / OSTIA rep / MUR 4.1 / MUR 4.2
-source = 'OSTIA v2';
+source = 'OSTIA rep';
 
-period = [2012, 1, 1; 2023, 12, 31];
-Spatial_area = [24, 35, 121, 132];
+period = [1982, 1, 1; 2023, 12, 31];
+Spatial_area = [22 54, 118, 144]; % Adjacent shelf sea around the Korean Peninsula
 
 % If the downloaded file has the SAME NAME, INCREASE the value of lag_time
 lag_time = 3; % seconds between download each files
